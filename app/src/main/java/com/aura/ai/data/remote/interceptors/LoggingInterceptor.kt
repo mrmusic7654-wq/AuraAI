925bddc788466202391a83b59c790c3fa3eb1dd0
mrmusic7654-wq <mr.music7654@gmail.com>
2026-04-17 21:38:59 +0545

    Update LoggingInterceptor.kt

--- app/src/main/java/com/aura/ai/data/remote/interceptors/LoggingInterceptor.kt
+++ app/src/main/java/com/aura/ai/data/remote/interceptors/LoggingInterceptor.kt
@@ -1,3 +1,25 @@
-package com.aura.ai.data.remote.interceptors.LoggingInterceptor
+package com.aura.ai.data.remote.interceptors
 
-// TODO: Implement
+import okhttp3.Interceptor
+import okhttp3.Response
+import okhttp3.logging.HttpLoggingInterceptor
+import javax.inject.Inject
+
+class LoggingInterceptor @Inject constructor() : Interceptor {
+    
+    private val logger = HttpLoggingInterceptor { message ->
+        if (BuildConfig.DEBUG) {
+            android.util.Log.d("AuraAPI", message)
+        }
+    }.apply {
+        level = if (BuildConfig.DEBUG) {
+            HttpLoggingInterceptor.Level.BODY
+        } else {
+            HttpLoggingInterceptor.Level.NONE
+        }
+    }
+    
+    override fun intercept(chain: Interceptor.Chain): Response {
+        return logger.intercept(chain)
+    }
+}
