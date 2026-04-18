70c8425cb801a664fae2f2303c35c812d06e2d3d
mrmusic7654-wq <mr.music7654@gmail.com>
2026-04-18 05:53:50 +0545

    Update AuthenticateGitHubUseCase.kt

--- app/src/main/java/com/aura/ai/domain/usecases/github/AuthenticateGitHubUseCase.kt
+++ app/src/main/java/com/aura/ai/domain/usecases/github/AuthenticateGitHubUseCase.kt
@@ -1,3 +1,14 @@
-package com.aura.ai.domain.usecases.github.AuthenticateGitHubUseCase
+package com.aura.ai.domain.usecases.github
 
-// TODO: Implement
+import com.aura.ai.data.models.github.GitHubUser
+import com.aura.ai.domain.repository.GitHubRepository
+import javax.inject.Inject
+
+class AuthenticateGitHubUseCase @Inject constructor(
+    private val repository: GitHubRepository
+) {
+    
+    suspend operator fun invoke(token: String): Result<GitHubUser> {
+        return repository.authenticate(token)
+    }
+}
