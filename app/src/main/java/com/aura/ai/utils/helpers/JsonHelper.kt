ea4cbd7f1c0526ccb5b79cc07dcb2222099e60cf
mrmusic7654-wq <mr.music7654@gmail.com>
2026-04-19 09:21:38 +0545

    Update JsonHelper.kt

--- app/src/main/java/com/aura/ai/utils/helpers/JsonHelper.kt
+++ app/src/main/java/com/aura/ai/utils/helpers/JsonHelper.kt
@@ -1,3 +1,25 @@
-package com.aura.ai.utils.helpers.JsonHelper
+package com.aura.ai.utils.helpers
 
-// TODO: Implement
+import kotlinx.serialization.json.Json
+
+object JsonHelper {
+    
+    val json = Json {
+        ignoreUnknownKeys = true
+        coerceInputValues = true
+        encodeDefaults = true
+        prettyPrint = true
+    }
+    
+    inline fun <reified T> fromJson(jsonString: String): T? {
+        return try {
+            json.decodeFromString<T>(jsonString)
+        } catch (e: Exception) {
+            null
+        }
+    }
+    
+    inline fun <reified T> toJson(data: T): String {
+        return json.encodeToString(data)
+    }
+}
