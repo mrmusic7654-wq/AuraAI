016dd507ceb4564d9e765dbfdeefdc5eec506c8e
mrmusic7654-wq <mr.music7654@gmail.com>
2026-04-19 09:19:26 +0545

    Update ActionTypes.kt

--- app/src/main/java/com/aura/ai/utils/constants/ActionTypes.kt
+++ app/src/main/java/com/aura/ai/utils/constants/ActionTypes.kt
@@ -1,3 +1,14 @@
-package com.aura.ai.utils.constants.ActionTypes
+package com.aura.ai.utils.constants
 
-// TODO: Implement
+object ActionTypes {
+    const val TAP = "TAP"
+    const val SWIPE = "SWIPE"
+    const val TYPE = "TYPE"
+    const val BACK = "BACK"
+    const val HOME = "HOME"
+    const val RECENTS = "RECENTS"
+    const val WAIT = "WAIT"
+    const val SCREENSHOT = "SCREENSHOT"
+    const val OPEN_APP = "OPEN_APP"
+    const val FIND_AND_TAP = "FIND_AND_TAP"
+}
