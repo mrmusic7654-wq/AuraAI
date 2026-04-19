38c860e4f2bab5d22e446972798904e89a3a024b
mrmusic7654-wq <mr.music7654@gmail.com>
2026-04-19 08:56:45 +0545

    Update SaveApiKeyUseCase.kt

--- app/src/main/java/com/aura/ai/domain/usecases/settings/SaveApiKeyUseCase.kt
+++ app/src/main/java/com/aura/ai/domain/usecases/settings/SaveApiKeyUseCase.kt
@@ -1,3 +1,17 @@
-package com.aura.ai.domain.usecases.settings.SaveApiKeyUseCase
+package com.aura.ai.domain.usecases.settings
 
-// TODO: Implement
+import com.aura.ai.data.local.preferences.AuraPreferences
+import javax.inject.Inject
+
+class SaveApiKeyUseCase @Inject constructor(
+    private val preferences: AuraPreferences
+) {
+    
+    operator fun invoke(apiKey: String) {
+        preferences.saveApiKey(apiKey)
+    }
+    
+    suspend fun saveToDataStore(apiKey: String) {
+        preferences.saveApiKeyToDataStore(apiKey)
+    }
+}
