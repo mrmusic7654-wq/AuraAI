e927aa067466f7812959eabddde0ff86486f83bf
mrmusic7654-wq <mr.music7654@gmail.com>
2026-05-01 03:26:41 +0545

    Update LoggingInterceptor to use BASIC logging level
    Change logging level to BASIC for production use.

--- app/src/main/java/com/aura/ai/data/remote/interceptors/LoggingInterceptor.kt
+++ app/src/main/java/com/aura/ai/data/remote/interceptors/LoggingInterceptor.kt
@@ -6,19 +6,13 @@ import okhttp3.logging.HttpLoggingInterceptor
 import javax.inject.Inject
 
 class LoggingInterceptor @Inject constructor() : Interceptor {
-    
+
     private val logger = HttpLoggingInterceptor { message ->
-        if (BuildConfig.DEBUG) {
-            android.util.Log.d("AuraAPI", message)
-        }
+        android.util.Log.d("AuraAPI", message)
     }.apply {
-        level = if (BuildConfig.DEBUG) {
-            HttpLoggingInterceptor.Level.BODY
-        } else {
-            HttpLoggingInterceptor.Level.NONE
-        }
+        level = HttpLoggingInterceptor.Level.BASIC
     }
-    
+
     override fun intercept(chain: Interceptor.Chain): Response {
         return logger.intercept(chain)
     }
