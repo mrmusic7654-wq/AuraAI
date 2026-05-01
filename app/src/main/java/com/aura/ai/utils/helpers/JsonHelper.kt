99d88e9e73e0bcddb444e7b707aab3b7c4d0aa4d
mrmusic7654-wq <mr.music7654@gmail.com>
2026-05-02 02:23:35 +0545

    Refactor JsonHelper to remove prettyPrint and update fromJson
    Removed prettyPrint option and updated fromJson to return non-nullable type.

--- app/src/main/java/com/aura/ai/utils/helpers/JsonHelper.kt
+++ app/src/main/java/com/aura/ai/utils/helpers/JsonHelper.kt
@@ -1,24 +1,20 @@
 package com.aura.ai.utils.helpers
 
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.encodeToString
 
 object JsonHelper {
-    
+
     val json = Json {
         ignoreUnknownKeys = true
         coerceInputValues = true
         encodeDefaults = true
-        prettyPrint = true
     }
-    
-    inline fun <reified T> fromJson(jsonString: String): T? {
-        return try {
-            json.decodeFromString<T>(jsonString)
-        } catch (e: Exception) {
-            null
-        }
+
+    inline fun <reified T> fromJson(jsonString: String): T {
+        return json.decodeFromString(jsonString)
     }
-    
+
     inline fun <reified T> toJson(data: T): String {
         return json.encodeToString(data)
     }
