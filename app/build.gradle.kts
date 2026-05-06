15f09425871a22c426fd4a761ddf13ab9882897b
mrmusic7654-wq <mr.music7654@gmail.com>
2026-05-06 09:15:48 +0545

    Remove testInstrumentationRunner from build.gradle.kts
    Removed testInstrumentationRunner from build configuration.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,6 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0.0"
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
     }
 
